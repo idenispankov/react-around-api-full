ad3457317ce8eaac24b94997c0db0fec9281f672
Denis Pankov <idenispankov@gmail.com>
2021-04-02 15:34:35 -0700

    added email and password to User Schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,6 +2,21 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: "You must provide a valid email address.",
+    },
+  },
+  password: {
+    type: String,
+    minlength: 10,
+    required: true,
+    select: false,
+  },
   name: {
     type: String,
     minLength: 2,
