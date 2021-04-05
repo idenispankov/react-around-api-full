6159264c09c985c03cf05cfb7a87ed97904c301f
Denis Pankov <idenispankov@gmail.com>
2021-04-05 11:10:22 -0700

    hash password on create

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    minLength: 2,
+    maxLength: 30,
+    default: "Jacques Cousteau",
+  },
+  about: {
+    type: String,
+    minLength: 2,
+    maxLength: 30,
+    default: "Explorer",
+  },
+  avatar: {
+    type: String,
+    default: "https://pictures.s3.yandex.net/resources/avatar_1604080799.jpg",
+    validate: {
+      validator: (v) => validator.isURL(v, { protocols: ["http", "https"] }),
+      message: "You must provide a valid URL for the user avatar.",
+    },
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
