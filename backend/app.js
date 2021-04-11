bce731c2609c0b2603d9694c8a42a859d5724d36
Denis Pankov <idenispankov@gmail.com>
2021-04-11 13:53:25 -0700

    errors fixed

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,8 @@ const { requestLogger, errorLogger } = require("./middleware/logger");
 const usersRouter = require("./routes/users");
 const cardsRouter = require("./routes/cards");
 const { createUser, login } = require("./controllers/usersController");
+const conflictError = require("./errors/conflictError");
+const NotFoundError = require("./errors/NotFoundError");
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -67,6 +69,12 @@ app.use("/", auth, cardsRouter);
 
 app.use(errorLogger);
 app.use(errors());
+app.use(conflictError);
+
+app.use("*", (req, res, next) => {
+  next(new NotFoundError("Requested resource not found"));
+});
+
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
