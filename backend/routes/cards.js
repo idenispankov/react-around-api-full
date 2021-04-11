45daf86b45ca76401505ac5d33a2f4d9025a303c
Denis Pankov <idenispankov@gmail.com>
2021-04-11 13:34:46 -0700

    temporary removed front end part, added Joi Validation to cards router

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { celebrate, Joi } = require("celebrate");
 
 const router = express.Router();
 const {
@@ -17,15 +18,50 @@ router.get("/cards", getCards);
 router.get("/cards/:id", getSingleCard);
 
 // Create Card
-router.post("/cards", createCard);
+router.post(
+  "/cards",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string()
+        .uri({ scheme: ["http", "https"] })
+        .required(),
+    }),
+  }),
+  createCard
+);
 
 // Delete Card
-router.delete("/cards/:cardId", deleteCard);
+router.delete(
+  "/cards/:cardId",
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24),
+    }),
+  }),
+  deleteCard
+);
 
 // Like Card
-router.put("/cards/:cardId/likes", likeCard);
+router.put(
+  "/cards/:cardId/likes",
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24),
+    }),
+  }),
+  likeCard
+);
 
 // Dislike Card
-router.delete("/cards/:cardId/likes", dislikeCard);
+router.delete(
+  "/cards/:cardId/likes",
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().hex().length(24),
+    }),
+  }),
+  dislikeCard
+);
 
 module.exports = router;
--- frontend/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className='footer'>
-      <p className='footer__text'>&copy; 2020 Around The U.S.</p>
-    </footer>
-  );
-}
